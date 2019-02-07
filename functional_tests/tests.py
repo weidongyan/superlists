38d226b056eb738d04cff6449f0a416343f88ce2
root <root@python.fcg.ai>
2019-02-07 21:33:14 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.brower = webdriver.Firefox()
@@ -11,8 +12,13 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.brower.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.brower.get('http://localhost:8000')
+        self.brower.get(self.live_server_url)
         self.assertIn('To-Do', self.brower.title)
         header_text = self.brower.find_element_by_tag_name('h1').text
         inputbox = self.brower.find_element_by_id('id_new_item')
@@ -23,13 +29,15 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        table = self.brower.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peack feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        #self.check_for_row_in_list_table('1: Buy peacock feathers')
+        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feather to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
