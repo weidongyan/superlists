0db5d4c8906006d8f03e5c4e3026b0e4387a9619
root <root@python.fcg.ai>
2019-02-19 10:35:03 +0800

    Use get_absolute_url on List model to DRY urls in views

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -4,7 +4,7 @@ class LayoutAndStylingTest(FunctionalTest):
 
     def test_layout_and_styling(self):
         self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
+        self.browser.set_window_size(1440, 900)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
